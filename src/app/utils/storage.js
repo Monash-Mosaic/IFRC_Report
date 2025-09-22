d6d8931af3ca742902d171f7832316e8ed17145f
Aditya Patel <apat0080@student.monash.edu>
2025-09-22 15:36:59 +1000

    generalise get & set data funcs

--- src/app/utils/storage.js
+++ src/app/utils/storage.js
@@ -1,28 +1,31 @@
 import 'client-only'
-export const STORAGE_KEY = "ifrcReport:bookmarkData"
 
-function getData() {
+export const BOOKMARK_KEY = "ifrcReport:bookmarkData"
+
+
+function getData(key, fallback) {
   if (typeof window === "undefined") {
-    console.log("localStorage is not available on the server side")
-    return {}
+    console.error("localStorage is not available on the server side")
+    return fallback
   }
-  const raw = localStorage.getItem(STORAGE_KEY)
-  return raw ? JSON.parse(raw) : { bookmarks: {} }
+  const raw = localStorage.getItem(key)
+  return raw ? JSON.parse(raw) : fallback
 }
 
-function setData(data) {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
+function setData(data, key) {
+  localStorage.setItem(key, JSON.stringify(data))
 }
+
 
 export function getBookmarks() {
-  const data = getData()
+  const data = getData(BOOKMARK_KEY, { bookmarks: {} })
   return new Set(Object.keys(data.bookmarks).filter((key) => data.bookmarks[key]))
 }
 
 export function toggleBookmark(sectionName) {
-  const data = getData()
+  const data = getData(BOOKMARK_KEY, { bookmarks: {} })
   data.bookmarks[sectionName] = !data.bookmarks[sectionName]
-  setData(data)
+  setData(data, BOOKMARK_KEY)
   return getBookmarks()
 }
 
