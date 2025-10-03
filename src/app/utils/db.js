6c81a323caf7f8eeaf9fde36dd21f2c139e06bd0
Aditya Patel <apat0080@student.monash.edu>
2025-10-03 21:17:36 +1000

    declare dexie db

---
+++ src/app/utils/db.js
@@ -0,0 +1,15 @@
+import Dexie from 'dexie';
+
+export const db = new Dexie("ifrcReportDB");
+
+db.version(1).stores({
+    bookmarks: '++id, sectionName',
+
+});
+
+/* TODO:
+  In the future, IndexedDB will hold the following:
+    - Highlights
+    - Proggress
+    - Notes
+*/
