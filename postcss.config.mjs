66596bca8eb38e9097653a5a811cc34e2a2306dd
Nathan Tran <kieron.qtran@gmail.com>
2025-12-16 11:38:14 +0700

    feat: add type of harm icons

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
 const config = {
-  plugins: ['@tailwindcss/postcss'],
+  plugins: [
+    '@tailwindcss/postcss',
+    'autoprefixer',
+    // 'cssnano', // Uncomment this line to enable CSS minification
+  ],
 };
 
 export default config;
