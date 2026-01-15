7d8ddee995a538852f198da3e8892bf1271dc7df
trietGoToTheZoo <trietbui2810@outlook.com>
2026-01-15 22:02:18 +1100

    overlay adjust

--- src/components/landing-page/HeroSection.js
+++ src/components/landing-page/HeroSection.js
@@ -12,7 +12,7 @@ export default function HeroSection({ messages }) {
       <div className="relative pt-8 pb-8 px-4 md:px-20 overflow-hidden rounded-lg min-h-[500px] md:min-h-[600px] ">
         <div className="absolute inset-0 w-full h-full overflow-hidden">
           <HeroVideo alt={messages.heroAlt} />
-          <div className="absolute inset-0 bg-black/30 z-20" />
+          <div className="absolute inset-0 bg-black/20 z-20" />
         </div>
         <div className="relative z-10 space-y-8">
           <div className="space-y-6">
