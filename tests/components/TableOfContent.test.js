b38d46812238050e1089533c658f45a0ceb4df4e
Nathan Tran <kieron.qtran@gmail.com>
2026-02-26 15:45:53 +0700

    more remove

--- tests/components/TableOfContent.test.js
+++ tests/components/TableOfContent.test.js
@@ -5,18 +5,6 @@ const mockChapterTableOfContents = [
   { depth: 1, id: 'introduction', value: 'Introduction' },
   { depth: 1, id: 'getting-started', value: 'Getting Started' },
   { depth: 1, id: 'installation', value: 'Installation' },
-  {
-    depth: 1,
-    id: 'endnotes',
-    value: 'Endnotes',
-    children: [
-      {
-        depth: 2,
-        id: 'endnotes-header',
-        value: 'Endnotes Header',
-      },
-    ],
-  },
 ];
 
 const mockTitle = 'On This Page';
@@ -52,8 +40,7 @@ describe('TableOfContent', () => {
       <TableOfContent chapterTableOfContents={mockChapterTableOfContents} title={mockTitle} />
     );
 
-    const [rootList] = screen.getAllByRole('list');
-    const listItems = Array.from(rootList.children).filter((child) => child.tagName === 'LI');
+    const listItems = screen.getAllByRole('listitem');
 
     // Verify correct number of items
     expect(listItems).toHaveLength(mockChapterTableOfContents.length);
@@ -62,16 +49,5 @@ describe('TableOfContent', () => {
     mockChapterTableOfContents.forEach((item, index) => {
       expect(listItems[index]).toHaveTextContent(item.value);
     });
-  });
-  test('renders all table of content items with empty endnotes children', () => {
-    render(
-      <TableOfContent chapterTableOfContents={mockChapterTableOfContents} title={mockTitle} />
-    );
-
-    const listItems = screen.getAllByRole('listitem');
-    const endnoteItem = listItems[listItems.length - 1];
-
-    // Verify correct number of items
-    expect(endnoteItem.children).toHaveLength(1);
   });
 });
