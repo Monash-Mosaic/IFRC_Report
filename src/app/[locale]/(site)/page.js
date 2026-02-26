33157ab06b6eef8039195dacec226fb0118c4da2
Nathan Tran <kieron.qtran@gmail.com>
2026-02-26 15:31:55 +0700

    revert page

--- src/app/[locale]/(site)/page.js
+++ src/app/[locale]/(site)/page.js
@@ -71,7 +71,7 @@ export default async function Home({ params }) {
   const reportModule = getVisibleReports(locale)?.wdr25;
   const testimonialsList = reportModule?.testimonialsList || [];
   const featuredVideos = reportModule?.featuredVideos || [];
-  const chapterSlug = reportUriMap['wdr25'].chapters['synthesis'].languages[locale];
+  const chapterSlug = reportUriMap['wdr25'].chapters['chapter-02'].languages[locale];
   // Executive Summary translations
   const executiveSummary = {
     title: t('landingPage.executiveSummary.title'),
@@ -94,6 +94,7 @@ export default async function Home({ params }) {
     }),
     downloadLink: reportModule.chapters[chapterSlug].downloadLink,
   };
+
   // Hero Section translations
   const heroMessage = {
     title: t('landingPage.heroSection.title'),
@@ -131,13 +132,13 @@ export default async function Home({ params }) {
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(homeJsonLd) }}
       />
-      <main className="max-w-full lg:max-w-8/10 py-4 mx-auto px-4 space-y-16">
-        <HeroSection messages={heroMessage} />
-        <ExecutiveSummarySection messages={executiveSummary} />
+      <main className="max-w-full md:max-w-8/10 py-4 mx-auto px-4 space-y-16">
+        <HeroSection locale={locale} messages={heroMessage} />
+        <ExecutiveSummarySection locale={locale} messages={executiveSummary} />
 
         {/* Featured Videos Section */}
         <div>
-          <h2 className="text-4xl lg:text-5xl font-bold mb-8 text-gray-900">
+          <h2 className="text-4xl md:text-5xl font-bold mb-8 text-gray-900">
             {t('landingPage.featuredVideos.title')}
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -154,7 +155,7 @@ export default async function Home({ params }) {
 
         {/* Citations Section */}
         <div>
-          <h2 className="text-4xl lg:text-5xl font-bold mb-8 text-gray-900">
+          <h2 className="text-4xl md:text-5xl font-bold mb-8 text-gray-900">
             {t('landingPage.testimonials.title')}
           </h2>
           <EmblaCarousel locale={locale} slideWidth={284} loop={false}>
