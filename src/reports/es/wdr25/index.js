db77063159a8a17307412844f46ff3686ae8343e
trietGoToTheZoo <trietbui2810@outlook.com>
2026-03-02 21:53:35 +0700

    fix build

--- src/reports/es/wdr25/index.js
+++ src/reports/es/wdr25/index.js
@@ -64,6 +64,22 @@ export const featuredVideos = [
 export const chapterRelease = false;
 
 export const chapters = {
+  síntesis: {
+    metadata: {
+      chapterKey: 'síntesis',
+      chapterNumber: -1,
+      chapterPrefix: 'Síntesis',
+    },
+    component: Synthesis.default,
+    title: Synthesis.title,
+    subtitle: Synthesis.subtitle,
+    tableOfContents: Synthesis.tableOfContents,
+    downloadLink:
+      'https://www.ifrc.org/sites/default/files/2021-05/20201116_WorldDisasters_Full.pdf',
+    thumbnail: '/wdr25/chapters/Synthesis.jpg',
+    thumbnailOverlay: 'red',
+    released: true,
+  },
   'capitulo-02': {
     metadata: {
       chapterKey: 'chapter-02',
