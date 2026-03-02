da7e3309cc711dc655055e37ff577fe6755bcb13
Sebastian Parakal Jose <spar0142@student.monash.edu>
2026-03-03 02:13:19 +1100

    remove carousel

--- src/components/EmblaCarousel.js
+++ src/components/EmblaCarousel.js
@@ -108,7 +108,7 @@ export default function EmblaCarousel({
     const timer = setTimeout(onSelect, 0);
 
     return () => clearTimeout(timer);
-  }, [locale, emblaApi, onSelect]);
+  }, [locale]);
 
   // Computed values
   const totalChildren = Array.isArray(children) ? children.length : 1;
