69da5152a60052135a8c4c7963b2bfdfd4044a01
Sebastian Parakal Jose <spar0142@student.monash.edu>
2026-03-04 03:13:23 +1100

    Fixed hero banner

--- src/components/engagement/DiscoverHero.js
+++ src/components/engagement/DiscoverHero.js
@@ -2,15 +2,15 @@ import Image from 'next/image';
 
 export default function DiscoverHero({ imageSrc = '/engagement/discover-banner.png', imageAlt = 'Discover the Interactive Playbook' }) {
   return (
-    <section className="w-full max-w-7xl mx-auto px-4 pt-6 pb-6 md:pt-8 md:pb-8">
-      <div className="relative w-full overflow-hidden rounded-lg bg-slate-100 min-h-[180px] sm:min-h-[220px] md:min-h-[280px] lg:min-h-[320px]">
+    <section className="w-full pt-6 pb-6 md:pt-8 md:pb-8">
+      <div className="relative w-full overflow-hidden bg-slate-100 min-h-[200px] sm:min-h-[260px] md:min-h-[320px] lg:min-h-[380px]">
         <Image
           src={imageSrc}
           alt={imageAlt}
           fill
-          className="object-contain object-center"
+          className="object-cover object-center"
           priority
-          sizes="(max-width: 1280px) 100vw, 1280px"
+          sizes="100vw"
         />
       </div>
     </section>
