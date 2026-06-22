69daa7aa000ec67ad87e25ddb3e5742400f0f861
Raveesh Yadav <67216858+Raveesh1505@users.noreply.github.com>
2026-06-23 08:51:36 +1000

    fix: cross chapter alignment fix (#283)

--- src/components/CustomComponents.js
+++ src/components/CustomComponents.js
@@ -132,7 +132,7 @@ export function SmallQuote({ children, ...props }) {
 export function SmallQuoteAuthor({ children, ...props }) {
   return (
     <div
-      className="ml-12 text-start font-extralight border-t-2 border-[#ee2435]"
+      className="ms-12 text-start font-extralight border-t-2 border-[#ee2435]"
       {...props}
     >
       {children}
