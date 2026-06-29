215214ace8098057db067b3a6dfc2866e448c37f
Raveesh Yadav <raveeshyadav8@outlook.com>
2026-06-29 21:22:37 +1000

    fix: run fix

--- scripts/ci/npm-ci.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if [[ "${GITHUB_ACTOR:-}" == "dependabot[bot]" ]]; then
-  npm install --package-lock-only --workspaces --include-workspace-root
-fi
-
-npm ci
